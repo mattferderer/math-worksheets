2b7e4da1be6bb60030300d79fbf83fe1801ea826
Matt Ferderer <matt@mattferderer.com>
2024-11-02 22:03:11 -0500

    add cloudflare pages

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,11 @@
 import { defineConfig } from "@solidjs/start/config";
 
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    preset: "cloudflare-pages",
+
+    rollupConfig: {
+      external: ["__STATIC_CONTENT_MANIFEST", "node:async_hooks"],
+    },
+  },
+});
